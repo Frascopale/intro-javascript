874479b20ed32fb4d9885e161838192394c980ae
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-16 12:44:20 +0200

    primo esempio di javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+window.console.log("Ho fatto il caffè");
+document.write("Ho fatto il caffè");
+var tazzina = 0;  
+let nuovatazzina = 0;
+const caffè = 23; //commento 
+/*hjhgjgjhhgj 
+jnjkmklmklm*/
+let lattuga = 0;
+let cipolla = 0.989;
+let bananre = "POIU";
+let zucchero = true; 
+
+if (cipolla > 2) {
+    zucchero= false;
+}  else {
+    cipolla++;  /* è come dire cipolla= cipolla + 1 */
+}
+
+document.write(cipolla + "<br>"); //a concatenato die var di tipo diverso 
+
+for (let i=0; i<100; i++) {
+    lattuga++;
+}
+
+while ( lattuga < 101) {
+    cipolla++;
+    lattuga++;
+}
+
+document.write(cipolla);
+
+let aglio= 1 + 3;
